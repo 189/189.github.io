89f8a0ea90c0df8b0369601305e0d31a9dd0a25d
wangshangwen <wangshangwen>
2020-10-09 11:41:56 +0800

    update qrcode demo

--- react-qrcode-scan/scan.tsx
+++ react-qrcode-scan/scan.tsx
@@ -2,60 +2,73 @@ import * as React from "react";
 import { QRScaner, Camera, CameraItem } from "react-qrcode-scan";
 import "./style.css";
 
-interface RenderProps { }
+interface RenderProps {}
 
 interface RenderState {
-    results: { date: number, content: string }[];
-    camera: CameraItem | null;
+  results: { date: number; content: string }[];
+  camera: CameraItem | null;
 }
 
-export default class QRReader extends React.Component<RenderProps, RenderState> {
-    state: RenderState = {
-        results: [],
-        camera: null
-    };
-    onError = (message) => console.error(message);
-    onScan = (content) => {
-        const results = this.state.results;
-        results.unshift({
-            date: Date.now(),
-            content
-        });
-        this.setState({
-            results
-        });
-    }
-    onStart = (camera) => {
-        this.setState({
-            camera
-        });
-    }
-    render() {
-        const { results, camera } = this.state;
-        return (
-            <Camera onError={this.onError}>
-                { cameras => cameras.length > 0 ?
-                    <div className="qrwrap">
-                        <div className="main">
-                            <h2>Scan QR Code</h2>
-                            <QRScaner onStart={this.onStart} onScan={this.onScan} camera={cameras[0]} onError={this.onError} />
-                        </div>
-                        <aside>
-                            <section>
-                                <h3>Camera:</h3>
-                                <div>{camera ? camera.name : ""}</div>
-                            </section>
-                            <section>
-                                <h3>Results:</h3>
-                                <ul>
-                                    {results.map(result => <li key={result.date}>{result.content}</li>)}
-                                </ul>
-                            </section>
-                        </aside>
-
-                    </div>
-                    : <div> No Camera</div>}
-            </Camera>
-        );
-    }
+export default class QRReader extends React.Component<
+  RenderProps,
+  RenderState
+> {
+  state: RenderState = {
+    results: [],
+    camera: null,
+  };
+  onError = (err: Error) => alert(err);
+  onScan = (content) => {
+    const results = this.state.results;
+    results.unshift({
+      date: Date.now(),
+      content,
+    });
+    this.setState({
+      results,
+    });
+  };
+  onStart = (camera) => {
+    this.setState({
+      camera,
+    });
+  };
+  render() {
+    const { results, camera } = this.state;
+    return (
+      <Camera onError={this.onError}>
+        {(cameras) =>
+          cameras.length > 0 ? (
+            <div className="qrwrap">
+              <div className="main">
+                <h2>Scan QR Code</h2>
+                <QRScaner
+                  onStart={this.onStart}
+                  onScan={this.onScan}
+                  camera={cameras[0]}
+                  onError={this.onError}
+                />
+              </div>
+              <aside>
+                <section>
+                  <h3>Camera:</h3>
+                  <div>{camera ? camera.name : ""}</div>
+                </section>
+                <section>
+                  <h3>Results:</h3>
+                  <ul>
+                    {results.map((result) => (
+                      <li key={result.date}>{result.content}</li>
+                    ))}
+                  </ul>
+                </section>
+              </aside>
+            </div>
+          ) : (
+            <div> No Camera</div>
+          )
+        }
+      </Camera>
+    );
+  }
 }
